43ed3fb0693745344cf0d4395d13155d330a68c8
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-04 06:16:55 -0700

    Learning: Context

--- 01-starting-project 8/src/App.jsx
+++ 01-starting-project 8/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import { CartContext } from './store/shopping-cart-context.jsx';
 
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -13,9 +15,7 @@ function App() {
     setShoppingCart((prevShoppingCart) => {
       const updatedItems = [...prevShoppingCart.items];
 
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
+      const existingCartItemIndex = updatedItems.findIndex((cartItem) => cartItem.id === id);
       const existingCartItem = updatedItems[existingCartItemIndex];
 
       if (existingCartItem) {
@@ -43,9 +43,7 @@ function App() {
   function handleUpdateCartItemQuantity(productId, amount) {
     setShoppingCart((prevShoppingCart) => {
       const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
+      const updatedItemIndex = updatedItems.findIndex((item) => item.id === productId);
 
       const updatedItem = {
         ...updatedItems[updatedItemIndex],
@@ -65,14 +63,23 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItem: handleAddItemToCart,
+    removeItem: handleUpdateCartItemQuantity,
+  };
+
   return (
-    <>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+    <CartContext.Provider value={ctxValue}>
+      <Header cart={shoppingCart} onUpdateCartItemQuantity={handleUpdateCartItemQuantity} />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
+    </CartContext.Provider>
   );
 }
 
---
+++ 01-starting-project 8/src/store/shopping-cart-context.jsx
@@ -0,0 +1,9 @@
+import { createContext, useContext } from "react";
+
+export const CartContext = createContext({
+    items: [],
+    totalAmount: 0,
+    addItem: (item) => {},
+    removeItem: (id) => {},
+    clearCart: () => {},
+})
